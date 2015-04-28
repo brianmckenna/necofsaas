db1cbab6e047cc2d423a3ba7c8a55f7db9abd6f2
Brian McKenna <bmckenna@asascience.com>
2015-04-28 15:19:09 -0400

    private IP update

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import boto.ec2
 import boto.utils
 import jinja2
+import json
 import time
 
 instance_metadata = boto.utils.get_instance_metadata()
@@ -9,7 +10,7 @@ placement         = instance_metadata.get('placement')
 availability_zone = placement.get('availability-zone')
 region            = availability_zone[:-1]
 connection        = boto.ec2.connect_to_region(region) # TODO, we can change region
-local_ipv4s       = instance_metadata.get('local-ipv4s')
+local_ipv4        = instance_metadata.get('local-ipv4')
 
 def master(id, requester):
     try:
@@ -17,15 +18,14 @@ def master(id, requester):
             meta = json.load(f)
             # user_data from template
             j2_env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
-            USER_DATA = j2_env.get_template('master.user_data').render(local_ipv4s=local_ipv4s, id=id)
-            # create a NetworkInterface in our VPC (with public IP)
+            USER_DATA = j2_env.get_template('master.user_data').render(local_ipv4=local_ipv4, id=id)
+            # create a NetworkInterface in our VPC
             interface = boto.ec2.networkinterface.NetworkInterfaceSpecification(
                 subnet_id                   = meta['SUBNET_ID'],
                 groups                      = meta['SECURITY_GROUP_IDS'],
-                associate_public_ip_address = True # TODO: don't need public IP
+                associate_public_ip_address = True # TODO: don't need public IP, for DEBUG
             )
             interfaces = boto.ec2.networkinterface.NetworkInterfaceCollection(interface)
-            # reserve an instance for MASTER node
             reservation = connection.run_instances(
                 image_id = meta['IMAGE_ID'],
                 key_name = meta['KEY_NAME'],
@@ -41,7 +41,6 @@ def master(id, requester):
                 while status == 'pending':
                     time.sleep(5)
                     status = instance.update()
-                # -- tags
                 instance.add_tag('Name', meta['INSTANCE_NAME'])
                 instance.add_tag('REQUESTER', requester)
                 #instance.add_tag("ENVIRONMENT", DEPLOYMENT_ENVIRONMENT) # DEVELOPMENT/INTEGRATION/STAGING/PRODUCTION
