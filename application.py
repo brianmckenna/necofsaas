f3f092b599113f1ab9979afa58689720dd70c46f
Brian McKenna <bmckenna@asascience.com>
2015-06-18 15:10:20 -0400

    flask app

--- application.py
+++ application.py
@@ -1,9 +1,26 @@
 import flask
+import flask.ext.login
+import glob
 import json
-#import utils
+import necofs.utils
 import traceback
 
 app = flask.Flask(__name__)
+app.secret_key = 'fb32e85e-f645-430e-bc15-8d1dedec5cb9'
+
+# Flask-Login
+login_manager = flask.ext.login.LoginManager()
+login_manager.init_app(app)
+
+def _offerings():
+    offerings = []
+    jsons = glob.glob('*.json')
+    for j in jsons:
+        print j
+        with open(j, 'r') as fp:
+            offering = json.load(fp)
+            offerings.append(offering)
+    return offerings
 
 @app.route('/<id>', methods=['GET'])
 def meta(id):
@@ -31,12 +48,33 @@ def volumes(id):
         return '', 204
 
 
-# TODO: async?
-@app.route('/<id>', methods=['POST'])
-def create(id):
-    requester = '@brianmckenna'
-    utils.master(id, requester)
-    return '', 200
+@app.route('/offering')
+def offering():
+    return 'offering', 200
+
+@app.route('/documentation')
+def documentation():
+    return 'documentation', 200
+
+@app.route('/logout')
+def logout():
+    return 'logout', 200
+
+@app.route('/execute/<id>')
+def execute(id):
+    requester = '@brianmckenna' # TODO: use Flask-Login for this
+    necofs.utils.create_master(id, requester) # TODO: async
+    return 'executing', 200
+
+@app.route('/logs')
+def logs():
+    return 'logs', 200
+
+@app.route('/')
+def index():
+    offerings = _offerings()
+    print offerings
+    return flask.render_template('index.html', offerings=offerings)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7000, debug=True)
--- necofs/utils.py
+++ necofs/utils.py
@@ -4,51 +4,59 @@ import jinja2
 import json
 import time
 
+# EC2 Instance metadata
 instance_metadata = boto.utils.get_instance_metadata()
 instance_id       = instance_metadata.get('instance-id')
 placement         = instance_metadata.get('placement')
 availability_zone = placement.get('availability-zone')
 region            = availability_zone[:-1]
 connection        = boto.ec2.connect_to_region(region) # TODO, we can change region
-local_ipv4        = instance_metadata.get('local-ipv4')
+local_ipv4        = instance_metadata.get('local-ipv4') # TODO: get port the Flask application is running on
 
-def master(id, requester):
+def create_master(id, requester):
+
     try:
+
+        # -- load configuration data via JSON
         with open('%s.json' % id, 'r') as f:
-            meta = json.load(f)
-            # user_data from template
-            j2_env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
-            USER_DATA = j2_env.get_template('master.user_data').render(local_ipv4=local_ipv4, id=id)
+            config = json.load(f)
+
+            # user_data (cloud-init) from template
+            jinja2_environment = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+            USER_DATA = jinja2_environment.get_template('master.user_data').render(local_ipv4=local_ipv4, id=id)
+
             # create a NetworkInterface in our VPC
             interface = boto.ec2.networkinterface.NetworkInterfaceSpecification(
-                subnet_id                   = meta['SUBNET_ID'],
-                groups                      = meta['SECURITY_GROUP_IDS'],
+                subnet_id                   = config['SUBNET_ID'],
+                groups                      = config['SECURITY_GROUP_IDS'],
                 associate_public_ip_address = True # TODO: don't need public IP, for DEBUG
             )
             interfaces = boto.ec2.networkinterface.NetworkInterfaceCollection(interface)
+
+            # create the Instance reservation
             reservation = connection.run_instances(
-                image_id = meta['IMAGE_ID'],
-                key_name = meta['KEY_NAME'],
+                image_id = config['IMAGE_ID'],
+                key_name = config['KEY_NAME'],
                 user_data = USER_DATA,
-                instance_type = meta['MASTER_INSTANCE_TYPE'],
+                instance_type = config['MASTER_INSTANCE_TYPE'],
                 instance_initiated_shutdown_behavior = 'terminate',
-                placement_group = meta['PLACEMENT_GROUP'],
+                placement_group = config['PLACEMENT_GROUP'],
                 network_interfaces = interfaces,
-                instance_profile_name = meta['IAM_ROLE']
+                instance_profile_name = config['IAM_ROLE']
             )
+
+            # -- loop until instance is running and update tags
             for instance in reservation.instances:
                 status = instance.update()
                 while status == 'pending':
                     time.sleep(5)
                     status = instance.update()
-                instance.add_tag('Name', meta['INSTANCE_NAME'])
+                instance.add_tag('Name', config['INSTANCE_NAME'])
                 instance.add_tag('REQUESTER', requester)
-                #instance.add_tag("ENVIRONMENT", DEPLOYMENT_ENVIRONMENT) # DEVELOPMENT/INTEGRATION/STAGING/PRODUCTION
-                print status
-                # after up and running, log in something, not sure yet what
-                if status == 'running':
-                    pass
-                else:
-                    pass
+
+
+            return reservation
+
     except:
+         # -- notify user of failed process
         pass
